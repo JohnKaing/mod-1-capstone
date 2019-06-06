afd1cda6bb42130b116127b7897b4ef70fce1b82
Steve Nemes <snemes@kent.edu>
2019-06-06 14:05:14 -0400

    push for teammates

--- 19_module-1-capstone/java/src/main/java/com/techelevator/view/Gum.java
+++ 19_module-1-capstone/java/src/main/java/com/techelevator/view/Gum.java
@@ -7,18 +7,12 @@ public class Gum extends Products {
 	public Gum(String name, double price) {
 		super(name, price);
 		
-
-	
-		Gum testGum;
-		testGum = new Gum("juicy", 1.50);
-		System.out.println("testGum is: " + testGum.getName() + testGum.getPrice());
 		
 	}
-	
-	
-	
-	// Gum Chiclets = new Gum("Chiclets", 0.95);
-	
+		public String getSound() {
+			return "Chew Chew, Yum!";
+				
+	}
 	
 }
 
--- 19_module-1-capstone/java/src/main/java/com/techelevator/view/testing.java
+++ 19_module-1-capstone/java/src/main/java/com/techelevator/view/testing.java
@@ -1,5 +1,13 @@
 package com.techelevator.view;
 
-public class testing {
+public class testing  {
+	public static void main(String[] args) {
 
+	Gum testGum;
+	testGum = new Gum("juicy", 1.50);
+	System.out.println("testGum is: " + testGum.getName() + testGum.getPrice());
+	
+//	Beverage testBeverage
+//	testBeverage = new Beverage("Pepsi", 1.50);
+}
 }
