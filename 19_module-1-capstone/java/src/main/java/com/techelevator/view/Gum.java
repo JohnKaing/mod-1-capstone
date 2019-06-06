428d7dda2a1299c1178b44ea81f6c66e8720aaa4
John Kaing <freekaingjon@gmail.com>
2019-06-06 12:22:42 -0400

    start of products

---
+++ 19_module-1-capstone/java/src/main/java/com/techelevator/view/Gum.java
@@ -0,0 +1,19 @@
+package com.techelevator.view;
+
+public class Gum extends Products {
+
+	
+	public Gum() {
+		super(getName,);
+	
+		System.out.println();
+	}
+
+	
+	/*
+public void items(String name, double price, String sound)
+	 */
+	
+	
+	
+}
---
+++ 19_module-1-capstone/java/src/main/java/com/techelevator/view/Products.java
@@ -0,0 +1,49 @@
+package com.techelevator.view;
+
+public abstract class Products {
+
+	private String name;
+	private double price = 0.00;
+	private String sound;
+
+
+	public Products(String name, double price) {
+		this.name = name;
+		this.price = price;
+		}
+
+	public String getSound( ) {
+		return sound;
+	}
+
+	/**
+	 * @return the name
+	 */
+	public String getName() {
+		return name;
+	}
+
+	/**
+	 * @param name the name to set
+	 */
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	/**
+	 * @return the price
+	 */
+	public double getPrice() {
+		return price;
+	}
+
+	/**
+	 * @param price the price to set
+	 */
+	public void setPrice(double price) {
+		this.price = price;
+	}
+
+}
+
+
