a5d28576e656d0bce64a005f481e03dc676df966
Steve Nemes <snemes@kent.edu>
2019-06-06 14:27:48 -0400

    push for chips before pulling inventoryimport

---
+++ 19_module-1-capstone/java/src/main/java/com/techelevator/view/Chips.java
@@ -0,0 +1,17 @@
+package com.techelevator.view;
+
+
+public class Chips extends Products {
+
+
+	public Chips(String name, double price) {
+		super(name, price);
+		
+		
+	}
+		public String getSound() {
+			return "Crunch Crunch, Yum!";
+				
+	}
+	
+}
